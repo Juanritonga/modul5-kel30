3f730aaec514c7de7bb44a40ab5527d7724da729
Juanda Ritonga <ritongajuan102@gmail.com>
2022-12-06 18:43:30 +0700

    first commit

---
+++ src/component/Data.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+      id: 1,
+      nama: "Juanda Ritonga",
+      nim: "21120120140150",
+      githubUsername:"JuanRitonga",
+      kelompok: "30"
+    },
+    {
+      id: 2,
+      nama: "Septian Agung Permana",
+      nim: "21120120140129",
+      githubUsername:"Septagung23",
+      kelompok: "30"
+    },
+    {
+      id: 3,
+      nama: "Naura Sharfina Azarine",
+      nim: "21120120140159",
+      githubUsername:"knimoni",
+      kelompok: "30"
+    },
+    {
+      id: 4,
+      nama: "Nabila Hana Saphira",
+      nim: "21120120140112",
+      githubUsername:"nabilahanas",
+      kelompok: "30"
+    }
+  ];
